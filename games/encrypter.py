7defa257049aa5f41534e3629465177fad75a316
s1145204 <1145204@student.hsleiden.nl>
2024-10-04 13:07:16 +0200

    added difficulties

--- games/encrypter.py
+++ games/encrypter.py
@@ -1,15 +1,30 @@
 # encryption HEX
+import random
 def encrypter(game_mode) -> bool:
-    hex_easy_options = {"YOU": "19-F-15"}
+    hex_easy_options = [{"YOU": "19-F-15"}, {"MINE":"D-9-E-5"}, {"DATA":"4-1-14-1"}]
 
-    hex_medium_options = {"I": "9",
+    hex_medium_options = [{"I": "9",
                           "SEE":"13-5-5",
-                          "YOU":"19-F-15"}
+                          "YOU":"19-F-15"},
+                          {"NOT":"E-F-14",
+                           "ENOUGH":"5-E-F-15-7-8",
+                           "TIME":"14-9-D-5"},
+                          {"NOT":"E-F-14",
+                           "SAFE":"13-1-6-5",
+                           "HERE":"8-13-12"}]
 
-    hex_hard_options = {"YOUR": "19-F-15-12",
+    hex_hard_options = [{"YOUR": "19-F-15-12",
                         "DATA": "4-1-14-1",
                         "IS": "9-13",
-                        "MINE":"D-9-E-5"}
+                        "MINE":"D-9-E-5"},
+                        {"TIME":"",
+                         "IS":"",
+                         "RUNNING":"",
+                         "OUT":""},
+                        {"QUIT":"11-15-9-14",
+                         "WHILE":"17-8-9-5",
+                         "YOU":"19-F-15",
+                         "CAN":"3-1-E"}]
     hex_options = {
         'easy': hex_easy_options,
         'medium':hex_medium_options,
@@ -18,7 +33,7 @@ def encrypter(game_mode) -> bool:
 
     attempts = 3
 
-    for answer, question in hex_options[game_mode].items():
+    for answer, question in random.choice(hex_options[game_mode]).items():
 
         while attempts > 0:
             print(question)
